fc19cd6a2cd16c8fe32992a9ba70a2556a6ff9cc
Lalith Kota <kotalalith@gmail.com>
2024-11-18 11:38:52 +0530

    Removed deduplicator module. (Moved to openg2p-registry repo)
    Signed-off-by: Lalith Kota <kotalalith@gmail.com>

--- g2p_registry_deduplication_deduplicator/README.md
+++
@@ -1,3 +0,0 @@
-# OpenG2P Registry Deduplication - Deduplicator
-
-Refer to https://docs.openg2p.org.
--- g2p_registry_deduplication_deduplicator/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Part of OpenG2P. See LICENSE file for full copyright and licensing details.
-from . import registrant
-from . import dedupe_config
-from . import res_config_settings
